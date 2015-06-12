manolo/gwt-api-generator

commit 70cdad539b53509da87f2e35f811f0359b60a2a1

Import widget events only when they exist. Fixes #2.

template/helpers.js
@@ -85,17 +85,20 @@ module.exports = {
       }
     }
   },
+  hasItems: function(array) {
+    return array == true;
+  },
   hasEvents: function() {
-    return !!this.events;
+    return this.hasItems(this.events);
   },
   hasAttributes: function() {
-    return !!this.attributes;
+    return this.hasItems(this.attributes);
   },
   hasProperties: function() {
-    return !!this.properties;
+    return this.hasItems(this.properties);
   },
   hasParams: function() {
-    return !!this.params;
+    return this.hasItems(this.params);
   },
   capitalizeFirstLetter: function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);

template/Widget.template
@@ -2,7 +2,9 @@
 package <%= ns %>.client.widget;
 
 import <%= ns %>.client.element.*;
+<% if (hasEvents()) { %>
 import <%= ns %>.client.widget.event.*;
+<% } %>
 import com.vaadin.components.gwt.polymer.client.PolymerWidget;
 import com.vaadin.components.gwt.polymer.client.webapi.*;
 import com.google.gwt.core.client.JsArray;
@@ -54,7 +56,7 @@ public class <%= className() %> extends <%= baseWidgetName() %> {
 
 <% if (hasAttributes()) _.forEach(attributes, function(attribute){ %>
     /**
-     * <%= getDescription('     ', atribute) %>
+     * <%= getDescription('     ', attribute) %>
      *
      * JavaScript Info:
      * @attribute <%= attribute.name %>

commit 259c5fb19de8539281bb16268dc0c584659a33af

Fix regression introduced in: 70cdad53

template/Element.template
@@ -18,7 +18,7 @@ public interface <%= elementClassName() %> extends HTMLElement {
 
 <% if (hasAttributes()) _.forEach(attributes, function(attribute){ %>
     /**
-     * <%= getDescription('     ', atribute) %>
+     * <%= getDescription('     ', attribute) %>
      *
      * JavaScript Info:
      * @attribute <%= attribute.name %>

template/helpers.js
@@ -95,7 +95,7 @@ module.exports = {
     }
   },
   hasItems: function(array) {
-    return array == true;
+    return array && array.length > 0;
   },
   hasEvents: function() {
     return this.hasItems(this.events);