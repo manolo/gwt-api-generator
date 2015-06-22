207c9b8a39e935e64582c6b1498e6442a3ff5016
Manolo Carrasco <manolo@apache.org>
2015-06-22 10:30:58 +0200

    Restore setAttribute names which is clearer and the ability to pass values

--- lib/com/vaadin/polymer/PolymerWidget.java
+++ lib/com/vaadin/polymer/PolymerWidget.java
@@ -3,6 +3,8 @@ package com.vaadin.polymer;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
@@ -51,9 +53,20 @@ public class PolymerWidget extends HTMLPanel {
         getElement().setAttribute("aria-label", label);
     }
 
-    public void setMarkerAttributes(String attributes) {
-        for (String attr : attributes.trim().split(" ")) {
-            setBooleanAttribute(attr, true);
+    /**
+     * @param attributes a list of attributes or attribute:values pairs.
+     *        examples:
+     *          setAttributes("foo bar")
+     *          setAttributes("foo:bar ; hello:bye")
+     */
+    public void setAttributes(String attributes) {
+        for (String attr : attributes.trim().replace(" *([;:]) *", "$1").split("[; ]+")) {
+            MatchResult e = RegExp.compile(" *(\\w+)( *: *)?(\\w*)? *").exec(attr);
+            if (e.getGroup(3) != null) {
+                getElement().setAttribute(e.getGroup(1), e.getGroup(3));
+            } else {
+                setBooleanAttribute(e.getGroup(1), true);
+            }
         }
     }
 
