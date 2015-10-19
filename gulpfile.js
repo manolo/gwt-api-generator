c6fe0141da8e5c3d847dc43ba2f0dd2bbce7c4b2
Sauli Tähkäpää <sauli@vaadin.com>
2015-10-19 11:33:14 +0300

    Update references to vaadin-elements.

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('parse', ['analyze'], function(cb) {
 
 gulp.task('analyze', ['clean:target', 'pre-analyze'], function() {
   return gulp.src([globalVar.bowerDir + "*/*.html",
-    // vaadin components
+    // vaadin elements
     globalVar.bowerDir + "*/vaadin-*/vaadin-*.html",
     // ignore all demo.html, index.html and metadata.html files
     "!" + globalVar.bowerDir + "*/*demo.html",
