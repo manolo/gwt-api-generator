33b009fd7c12b796fdc3646043cf06314957d66f
Ahmad Bawaneh <akabme@gmail.com>
2017-08-30 10:12:13 +0300

    use elemental2

--- lib/com/vaadin/polymer/elemental/EventTarget.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.polymer.elemental;
-
-import static jsinterop.annotations.JsPackage.GLOBAL;
-import jsinterop.annotations.JsType;
-
-@JsType(isNative=true, namespace=GLOBAL)
-public interface EventTarget {
-    void addEventListener(String type, EventListener listener);
-}
--- lib/com/vaadin/polymer/elemental/Function.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.polymer.elemental;
-
-import jsinterop.annotations.JsFunction;
-
-@JsFunction
-public interface Function<RET, ARG>  {
-    public RET call(ARG arg);
-}
-
