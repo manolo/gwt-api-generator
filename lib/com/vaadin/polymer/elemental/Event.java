51837fda3f0266fe7c2cfd335b53e1b412555741
Ahmad Bawaneh <akabme@gmail.com>
2017-08-31 22:57:42 +0300

    fixing the events

--- lib/com/vaadin/polymer/elemental/Event.java
+++ lib/com/vaadin/polymer/elemental/Event.java
@@ -2,11 +2,22 @@ package com.vaadin.polymer.elemental;
 
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
+import elemental2.dom.EventInit;
+import elemental2.dom.EventTarget;
 
 import static jsinterop.annotations.JsPackage.GLOBAL;
 
 @JsType(isNative=true, namespace=GLOBAL, name="Event")
 public class Event extends elemental2.dom.Event{
+
+    public Event(String type, EventInit eventInitDict) {
+        super(type, eventInitDict);
+    }
+
+    public Event(String type) {
+        super(type);
+    }
+
     @JsProperty
     public native String getType();
 
